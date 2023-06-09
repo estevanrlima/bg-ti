a65138c34bd486c32f900614b3f704ad2422b381
estevan.lima <estevanlima16@gmail.com>
2023-06-09 15:30:30 -0300

    Login page created

--- bg/urls.py
+++ bg/urls.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from home import views as home_views
 from had import views as had_views
 from tests import views as tests_views
 from resiliteste import views as resi_views
 from burnout import views as burnout_views
+from authentication.views import login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_views.index),
-    path('had/', had_views.index),
-    path('tests/', tests_views.index),
-    path('resiliteste', resi_views.index),
-    path('burnout', burnout_views.index),
+    path('', home_views.index, name='home'),
+    path('had/', had_views.index, name='had'),
+    path('tests/', tests_views.index, name='tests'),
+    path('resiliteste', resi_views.index, name='resiliteste'),
+    path('burnout', burnout_views.index, name='burnout'),
+    path('login/', login_view, name='login'),
 ]
